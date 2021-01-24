f8220ced9397e33c769918a75823f8975e8de839
Rogerio Carvalho <bb.roger.bb@gmail.com>
2021-01-23 21:11:24 -0300

    implementação do middleaware de autenticação nas rotas dos posts - Aula 19

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
